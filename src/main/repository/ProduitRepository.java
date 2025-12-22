83b12de39de9d8b1be31e9be4fbc98d2f9f65937
unknown <lucas.cherbuin@gmail.com>
2025-12-22 13:12:38 +0100

    ProduitRepository.java

--- src/main/repository/ProduitRepository.java
+++ src/main/repository/ProduitRepository.java
@@ -1,8 +1,20 @@
 package maven.repository;
 
 import maven.model.Produit;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProduitRepository extends JpaRepository<Produit, Integer> {
-    
+   
+
+    List<Produit> findByNomContaining(String nom);
+    List<Produit> findByPrixLessThanEqual(int prix);
+    List<Produit> findByQuantiteStockGreaterThanEqual(int quantiteStock);
+    List<Produit> findByDerniereAjoutAfter(java.sql.Date date);
+    List<Produit> findByDatePeremptionBefore(java.sql.Date date);
+    List<Produit> findByPerissable(boolean perissable);
+    List<Produit> findByFournisseurId(int fournisseurId);
+    List<Produit> findByImageId(int imageId);
 }
