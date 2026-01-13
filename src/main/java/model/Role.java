dcfe2d0646b543f9c6407c7e6f10921c117c3874
unknown <lucas.cherbuin@gmail.com>
2026-01-13 12:25:18 +0100

    fix

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -1,29 +1,33 @@
 package model;
 
-public class Role {  
-    private int id;
-    private String label;
-    
-    public Role(int id, String label) {  
-        this.id = id;
-        this.label = label;
-    }
-    
-    // Getters
-    public int getId() {
-        return id;
-    }
-    
-    public String getLabel() {
-        return label;
-    }
-    
-    // Setters
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public void setLabel(String label) {
-        this.label = label;
-    }
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role { 
+ private int id;
+ private String label;
+ 
+ public Role(int id, String label) { 
+ this.id = id;
+this.label = label;
+}
+
+ // Getters
+public int getId() {
+return id;
+}
+
+ public String getLabel() {
+ return label;
+ }
+
+ // Setters
+public void setId(int id) {
+this.id = id;
+ }
+
+ public void setLabel(String label) {
+ this.label = label;
+}
 }
