bcec74e3b366b76e84d82b38656a1f20d4ebc847
unknown <lucas.cherbuin@gmail.com>
2026-04-07 10:47:49 +0200

    configuration mailer api backend

---
+++ src/main/java/com/maven/Service/mailer/MailService.java
@@ -0,0 +1,50 @@
+package com.maven.service.mailer;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+
+
+public class MailService implements MailServiceInterface {
+
+    private static final String SUCCESS_MESSAGE =
+            "Vous allez recevoir un email de confirmation.";
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+            System.out.print(subject);
+        Properties smtpProperties = new Properties();
+        smtpProperties.put("mail.smtp.auth", "true");
+        smtpProperties.put("mail.smtp.starttls.enable", "true");
+        smtpProperties.put("mail.smtp.host", System.getenv("MAILER_HOST"));
+        smtpProperties.put("mail.smtp.port", System.getenv("MAILER_PORT"));
+
+        try {
+            Session session = Session.getInstance(smtpProperties,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(
+                            System.getenv("MAILER_USERNAME"),
+                            System.getenv("MAILER_PASSWORD")
+                        );
+                    }
+                }
+            );
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(System.getenv("MAILER_USERNAME")));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println(SUCCESS_MESSAGE);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
