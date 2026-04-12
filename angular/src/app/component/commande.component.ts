c57ff4109c59f6291b1bb2fed5f4a626371e9132
unknown <lucas.cherbuin@gmail.com>
2026-04-12 17:11:28 +0200

    fix

--- angular/src/app/component/commande.component.ts
+++ angular/src/app/component/commande.component.ts
@@ -3,35 +3,37 @@ import { CommandeService } from '../services/commande.service';
 import { Commande } from '../models/commande.model';
 
 
+
     export class CommandeComponent implements OnInit {
 
     commandes: Commande[] = [];
     searchTerm: string = '';
 
-        constructor(private commandeService: CommandeService) {}
+
+        constructor(private commandeService: CommandeService authService: AuthService, ) {}
 
         ngOnInit(): void {
             this.loadCommandes();
         }
 
         loadCommandes(): void {
-        this.commandeService.getCommandes().subscribe(
-            (data: Commande[]) => {
-            this.commandes = data;
-            },
-            (error: any) => {
-            console.error('Error fetching commandes:', error);
-            }
-        );
-    }   
-
-    onSearch() {
-        if (this.searchTerm.trim() === '') {
-            this.loadCommandes();
-        } else {
-            this.commandeService.searchCommande(this.searchTerm).subscribe(data => {
+            this.commandeService.getCommandes().subscribe(
+                (data: Commande[]) => {
                 this.commandes = data;
-            }); 
+                },
+                (error: any) => {
+                console.error('Error fetching commandes:', error);
+                }
+            );
+        }   
+
+        onSearch() {
+            if (this.searchTerm.trim() === '') {
+                this.loadCommandes();
+            } else {
+                this.commandeService.searchCommande(this.searchTerm).subscribe(data => {
+                    this.commandes = data;
+                }); 
+            }
         }
-    }
 }
