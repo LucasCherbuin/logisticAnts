a16f8a3bf6bfc27c9e34eb8edcd8bbe76e8fb73d
unknown <lucas.cherbuin@gmail.com>
2026-04-21 17:23:30 +0200

    correction fichier pour builds

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { Routes } from 'app/routes';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(Routes)
+    provideRouter(routes)
   ]
 };
