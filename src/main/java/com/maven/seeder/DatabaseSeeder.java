11d9c9246fb66f29d08b82b2ed6b0397fa23fd34
LucasCherbuin <lucas.cherbuin@gmail.com>
2026-05-26 21:44:41 +0200

    ajout seeder PageClient

--- src/main/java/com/maven/seeder/DatabaseSeeder.java
+++ src/main/java/com/maven/seeder/DatabaseSeeder.java
@@ -1,26 +1,35 @@
 package com.maven.seeder;
 
-import com.maven.model.Role;
-
-import com.maven.repository.RoleRepository;
-import com.maven.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component 
+@Component
 public class DatabaseSeeder implements CommandLineRunner {
+
     private final RoleSeeder roleSeeder;
     private final UserSeeder userSeeder;
+    private final PaymentMethodSeeder payementMethodSeeder;
+    private final ArticleCommandeSeeder articleCommandeSeeder;
+    private final CommandeSeeder commandeSeeder;
+    private final ProduitSeeder produitSeeder;
 
-    public DatabaseSeeder(RoleSeeder roleSeeder, UserSeeder userSeeder) {
+    public DatabaseSeeder(RoleSeeder roleSeeder, UserSeeder userSeeder, PaymentMethodSeeder payementMethodSeeder, ArticleCommandeSeeder articleCommandeSeeder, CommandeSeeder commandeSeeder, ProduitSeeder produitSeeder) {
         this.roleSeeder = roleSeeder;
         this.userSeeder = userSeeder;
+        this.payementMethodSeeder = payementMethodSeeder;
+        this.articleCommandeSeeder = articleCommandeSeeder;
+        this.commandeSeeder = commandeSeeder;
+        this.produitSeeder = produitSeeder;
     }
 
     @Override
     public void run(String... args) throws Exception {
         roleSeeder.seedRoles();
         userSeeder.seedUsers();
+        payementMethodSeeder.seedPayementMethod();
+        articleCommandeSeeder.seedArticleCommandes();
+        commandeSeeder.seedCommandes();
+        produitSeeder.seedProduits();
         System.out.println("Seeding done");
     }
 }
