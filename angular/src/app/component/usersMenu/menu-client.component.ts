19085aca15a32937ebfa0e260636e5fc952da522
LucasCherbuin <lucas.cherbuin@gmail.com>
2026-05-31 10:52:00 +0200

    configuration logout

---
+++ angular/src/app/component/usersMenu/menu-client.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserMenuComponent } from './userMenu.component';
+import { RoleService } from '../../services/role.service';
+import { RegisterService } from '../../services/register.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+    selector: 'app-menu-client',
+    standalone: true,
+    imports: [CommonModule, UserMenuComponent],
+    templateUrl: '../../pages/userMenu/menu-client.component.html',
+    styleUrls: ["../../../main.scss"],
+})
+export class MenuClientComponent {
+    readonly CLIENT = 'CLIENT';
+    Role: string = '';
+
+    constructor(private roleService: RoleService, private registerService: RegisterService, private router: Router) {
+        this.roleService.getRoles();
+    }
+    logout() {
+        this.registerService.logout();
+        this.router.navigate(['/']);
+    }
+}
