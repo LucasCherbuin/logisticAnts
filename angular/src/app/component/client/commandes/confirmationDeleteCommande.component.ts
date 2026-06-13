31dbfcd0ed38bfd0fb36033d809c34ae167120e8
LucasCherbuin <lucas.cherbuin@gmail.com>
2026-06-14 00:08:47 +0200

    correction et test PageClient

--- angular/src/app/component/client/commandes/confirmationDeleteCommande.component.ts
+++ angular/src/app/component/client/commandes/confirmationDeleteCommande.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectorRef } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 @Component({
@@ -6,35 +6,39 @@ import { CommonModule } from "@angular/common";
     standalone: true,
     imports: [CommonModule],
     template: `
-                <div class="overlay" *ngIf="isVisible">
-                    <div class="popup">
-                        <p>Cette action est irréversible.</p>
-                        <p>Voulez-vous vraiment supprimer cette commande ?</p>
-                        <div class="actions">
-                            <button (click)="confirm()">Confirmer</button>
-                            <button (click)="cancel()">Annuler</button>
-                        </div>
-                    </div>
+        <div class="overlay" *ngIf="isVisible">
+            <div class="popup">
+                <p>Cette action est irréversible.</p>
+                <p>Voulez-vous vraiment supprimer cette commande ?</p>
+                <div class="actions">
+                    <button (click)="confirm()">Confirmer</button>
+                    <button (click)="cancel()">Annuler</button>
                 </div>
-            `,
+            </div>
+        </div>
+    `,
     styleUrls: ['../../../../main.scss'],
-    })
+})
+export class ConfirmationDeleteCommandeComponent {
+    isVisible: boolean = false;
+    private confirmCallback!: () => void;
 
-    export class ConfirmationDeleteCommandeComponent {
-        isVisible: boolean = false;
-        private confirmCallback!: () => void;
+    constructor(private cdr: ChangeDetectorRef) {}
 
-        open(onConfirm: () => void): void {
-            this.isVisible = true;
-            this.confirmCallback = onConfirm;
-        }
-
-        confirm(): void {
-            this.isVisible = false;
-            this.confirmCallback();
-        }
-
-        cancel(): void {
-            this.isVisible = false;
-        }
+    open(onConfirm: () => void): void {
+        this.isVisible = true;
+        this.confirmCallback = onConfirm;
+        this.cdr.detectChanges();
     }
+
+    confirm(): void {
+        this.isVisible = false;
+        this.cdr.detectChanges();
+        this.confirmCallback();
+    }
+
+    cancel(): void {
+        this.isVisible = false;
+        this.cdr.detectChanges();
+    }
+}
